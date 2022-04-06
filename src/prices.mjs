f2b52548ccfa76074d184ff42bd4c5ad8b1bc044
Ezequiel Omar <ezequiel.omar.vazquez@gmail.com>
2022-04-05 21:02:01 -0300

    refactor: changing old methods from Date, with new methods from Temporal.PlainDate

--- src/prices.mjs
+++ src/prices.mjs
@@ -93,12 +93,12 @@ function createApp(database) {
   function isHoliday(date) {
     const holidays = database.getHolidays();
     for (let row of holidays) {
-      let holiday = new Date(row.holiday);
+      let holiday = parseDateToTemporalDate(row.holiday);
       if (
         date &&
-        date.getFullYear() === holiday.getFullYear() &&
-        date.getMonth() === holiday.getMonth() &&
-        date.getDate() === holiday.getDate()
+        date.year === holiday.year &&
+        date.month === holiday.month &&
+        date.toString() === holiday.toString()
       ) {
         return true;
       }
